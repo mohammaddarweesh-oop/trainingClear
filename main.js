4d0dc29fd2e029ff6e7c9eceb202837351652327
Mohammad_Darweesh <77121631+mohammaddarweesh-oop@users.noreply.github.com>
2023-06-18 00:05:36 +0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,10 @@
+var div2 = document.getElementById('di');
+var one = getElementById("one").style.backgroundColor = "black";;
+div2.innerHTML = " Mohammad Darweesh";
+// one.style.backgroundColor = "red";
+var footer = document.getElementsByTagName("footer");
+footer.style.backgroundColor = "red";
+
+
+
+
